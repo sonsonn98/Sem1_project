9b2e2de89ee4422e7b69168dd5590d5f1f3e17ad
tuananh21 <chutuananh.vnu@gmail.com>
2024-07-23 15:49:32 +0700

    update

--- beautifulbeaches/app/controllers/AboutusController.php
+++ beautifulbeaches/app/controllers/AboutusController.php
@@ -2,13 +2,16 @@
 class AboutusController extends BaseController {
 
     private $__aboutusModel;
+    private $__homeModel;
     function __construct($conn)
     {
         $this->__aboutusModel= $this->initModel("AboutusModel",$conn);
+        $this->__homeModel= $this->initModel("HomeModel",$conn);
     }
     // action
     public function index(){
-        $this->view("layout", ["content" => "aboutus"]);
+        $zones = $this->__homeModel->getAllZones();
+        $this->view("layout", ["content" => "aboutus","zones"=>$zones]);
     }
 
 
