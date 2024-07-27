611efbbe24eedd0acc35afe90c4fb4596efafd1f
CHIEN MINH <chienminh1703@gmail.com>
2024-07-27 12:51:30 +0700

    update

--- beautifulbeaches/app/models/DetailsModel.php
+++ beautifulbeaches/app/models/DetailsModel.php
@@ -86,34 +86,35 @@ class DetailsModel
             echo $e->getMessage();
         } }
         
-    public function saveReviews($starValue, $beache_id, $name, $comment)
-    {
-        try {
-            $sql = "INSERT INTO beach_review (`rating`, `beach_id`, `reviewer_name`, `review_comments`) 
-                    VALUES (:rating, :beach_id, :reviewer_name, :review_comments)";
-            $stmt = $this->__conn->prepare($sql);
-            $stmt->bindParam(":rating", $starValue, PDO::PARAM_INT);
-            $stmt->bindParam(":beach_id", $beache_id, PDO::PARAM_INT);
-            $stmt->bindParam(":reviewer_name", $name, PDO::PARAM_STR);
-            $stmt->bindParam(":review_comments", $comment, PDO::PARAM_STR);
-            $stmt->execute();
-            header("location: http://localhost/beautifulbeaches/details/index");
-        } catch (PDOException $e) {
-            echo "error" . $e->getMessage();
+        public function saveReviews($starValue, $beach_id, $name, $comment)
+        {
+            try {
+                $sql = "INSERT INTO beach_review (`rating`, `beach_id`, `reviewer_name`, `review_comments`) 
+                VALUES (:rating, :beach_id, :reviewer_name, :review_comments)";
+                $stmt = $this->__conn->prepare($sql);
+                $stmt->bindParam(":rating", $starValue, PDO::PARAM_INT);
+                $stmt->bindParam(":beach_id", $beach_id, PDO::PARAM_INT);
+                $stmt->bindParam(":reviewer_name", $name, PDO::PARAM_STR);
+                $stmt->bindParam(":review_comments", $comment, PDO::PARAM_STR);
+                $stmt->execute();
+            } catch (PDOException $e) {
+                echo "error" . $e->getMessage();
+            }
         }
-    }
-
-    public function getAllReviews()
-    {
-        try {
-            $sql = "SELECT * FROM beach_review";
-            $stmt = $this->__conn->prepare($sql);
-            $stmt->execute();
-            return $stmt->fetchAll(PDO::FETCH_ASSOC);
-        } catch (PDOException $e) {
-            echo "error" . $e->getMessage();
+    
+        public function getAllReviews($beach_id)
+        {
+            try {
+                $sql = "select * from beach_review where beach_id = :beach_id order by rating desc";
+                $stmt = $this->__conn->prepare($sql);
+                $stmt->bindParam(":beach_id", $beach_id, PDO::PARAM_INT);
+                $stmt->execute();
+                $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                return $result;
+            } catch (PDOException $e) {
+                echo "error" . $e->getMessage();
+            }
         }
-    }
 
     public function getBeachDetails($beach_id)
     {
