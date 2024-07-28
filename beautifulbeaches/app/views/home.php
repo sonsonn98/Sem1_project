caf4d540ff7e2273e75add1f50645792fc85bbae
hndcmm245 <hoangan240505@gmail.com>
2024-07-28 21:24:22 +0700

    em them vid bien vao roi nhe anh tuan anh oi

--- beautifulbeaches/app/views/home.php
+++ beautifulbeaches/app/views/home.php
@@ -71,8 +71,7 @@
               <div class="ad-title">FEATURED VIDEO</div>
               <div class="ad-media">
                 <div class="ad-thumbnail">
-                  <iframe src="https://www.youtube.com/embed/BjIcqPRI87A&ab" frameborder="1" width="100%"
-                  height="250"></iframe>
+                <iframe width="470" height="250" src="https://www.youtube.com/embed/0ZBqnOeIxbQ?si=P9oFAgoC99nAuI-p&amp;start=296" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                 </div>
                 <div class="ad-content">
                   <p>Whitehaven Beach - Whitsunday Islands - Australia</p>
